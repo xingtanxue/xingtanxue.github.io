6fa28090e39dfb15a1c5e01b578b7287f03babbd
xingtanxue <xingtanxue@gmail.com>
2017-07-31 18:50:16 +0800

    添加js

---
+++ js/umeng.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1263189730'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1263189730%26online%3D1' type='text/javascript'%3E%3C/script%3E"));</script>
